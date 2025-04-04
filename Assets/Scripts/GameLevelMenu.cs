5b4e13452cd881785cbc9afed6a366221f528233
Julie <julie.seyier03@gmail.com>
2025-04-04 15:12:54 +0200

    feat: Add of textures for the planets of the level selection menu and add of script to make the planets clickable

--- Assets/Scripts/GameLevelMenu.cs
+++ Assets/Scripts/GameLevelMenu.cs
@@ -1,29 +1,8 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class GameLevelMenu : MonoBehaviour
 {
-    public Button planet1Button;
-    public Button planet2Button;
-    public Button planet3Button;
-
-    private void Start()
-    {
-        // PlayerPrefs.DeleteAll();
-        
-        int levelReached = PlayerPrefs.GetInt("LevelReached", 1);
-
-        planet1Button.interactable = true;
-        planet2Button.interactable = (levelReached >= 2);
-        planet3Button.interactable = (levelReached >= 3);
-    }
-
-    public void LoadLevel(string levelName)
-    {
-        SceneManager.LoadScene(levelName);
-    }
-
     public void BackToHome()
     {
         SceneManager.LoadScene("MainGameMenu");
@@ -38,15 +17,3 @@ public class GameLevelMenu : MonoBehaviour
         }
     }
 }
-
-// using UnityEngine;
-// TODO: Add this script to each planets scripts
-// public class Planet1 : MonoBehaviour
-// {
-//     public GameLevelMenu gameLevelMenu;
-
-//     private void CompleteLevel()
-//     {
-//         gameLevelMenu.CompleteLevel(1);
-//     }
-// }
